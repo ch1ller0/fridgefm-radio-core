df2aa6cf24bf4f4f190d9e52558e8983d0225c52
Kefir100 <petralmazov100@gmail.com>
2019-03-14 23:15:05 +0300

    minor fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Station } = require('./src/sound/Station');
-const logger = require('./src/utils/logger');
+const { logger } = require('./src/utils/logger');
 const soundMethods = require('./src/sound/methods/sound');
 
 module.exports = {
