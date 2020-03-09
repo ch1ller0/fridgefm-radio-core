b7a40837499846e788c4a40fed414cc6806482c1
Grigory Gorshkov <petralmazov100@gmail.com>
2020-03-09 15:28:33 +0300

    Public class test coverage (#27)
    Closes #2

--- src/types/public.h.ts
+++ src/types/public.h.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from 'express';
 import type { TrackI } from './Track.h';
-import type { PlaylistI, SortAlg } from './Playlist.h';
+import type { TrackList, SortAlg } from './Playlist.h';
 
 export interface StationI {
   // new @TODO di container
@@ -12,8 +12,8 @@ export interface StationI {
   rearrangePlaylist(from: number, to: number): void;
   connectListener(req: Request, res: Response, cb: () => void): void;
   // exposed events
-  on(event: 'start', listener: (pl: PlaylistI) => void): void;
+  on(event: 'start', listener: (pl: TrackList) => void): void;
   on(event: 'restart', listener: () => void): void;
   on(event: 'nextTrack', listener: (nextTrack: TrackI) => void): void;
-  on(event: 'error', listener: (err: Error) => never): void;
+  on(event: 'error', listener: (err: Error) => void): void;
 }
