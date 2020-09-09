f6ce1e6c1512f63c6d195f07499397a16736733c
ch1ller <petralmazov100@gmail.com>
2020-09-10 01:13:59 +0300

    change naming

--- src/sound/defaults/responseHeaders.ts
+++ src/sound/defaults/responseHeaders.ts
@@ -1,13 +1,13 @@
 
-// TODO add icy metaint https://github.com/Kefir100/radio-ch1ller/issues/16
+// TODO add icy metaint https://github.com/Kefir100/fridgefm-radio-core/issues/16
 export const responseHeaders = {
   'Cache-Control': 'no-cache,no-store,must-revalidate,max-age=0',
   'Content-Type': 'audio/mpeg',
   'icy-br': '56',
   'icy-genre': 'house',
   'icy-metaint': '0',
-  'icy-name': '@kefir100/radio-engine',
-  'icy-notice1': 'Live radio powered by https://www.npmjs.com/package/@kefir100/radio-engine',
+  'icy-name': '@fridgefm/radio-engine',
+  'icy-notice1': 'Live radio powered by https://www.npmjs.com/package/@fridgefm/radio-engine',
   'icy-pub': '0',
   'icy-url': 'https://',
 };
